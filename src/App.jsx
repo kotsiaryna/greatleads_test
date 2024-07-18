ebddecd3222da6074cbfbf1d80e284fa573ef078
Katsiaryna <katsiaryna.n@gmail.com>
2024-07-18 17:26:29 +0300

    feat: update upp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Form from './components/Form/Form'
-import List from './components/List/List'
 import {Divider} from "@nextui-org/react";
+import Form from './components/Form'
+import List from './components/List'
 import Menu from './components/Menu';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ModalButton from './components/ModalButton';
 
 function App() {
-
-
   return (
     <>
       <Header />
       <Divider className="my-4" />
       <div className='w-10/12 mx-auto'>
-        <Menu />
+      <ModalButton/>
+        <Menu /> 
         <Divider className="my-4" />
         <List />
         <Divider className="my-4" />
